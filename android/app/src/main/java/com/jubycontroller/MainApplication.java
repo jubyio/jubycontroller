ba7e70b64762c14269436d8c2acb9cea2b6e3144
Raphael Lichan <raphael.lichan@outlook.com>
2017-09-26 14:52:51 +0200

    remove react-native-vector-icons

--- android/app/src/main/java/com/jubycontroller/MainApplication.java
+++ android/app/src/main/java/com/jubycontroller/MainApplication.java
@@ -3,7 +3,6 @@ package com.jubycontroller;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -30,8 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
   };
